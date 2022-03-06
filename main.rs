4a2a028e78fb4a3d20f8ec4b8f409dd4bf250bae
baimamboukar <baimamboukar@gmail.com>
2022-03-06 20:44:17 +0100

    rust basics

---
+++ main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello World!");
+    great_ladies();
+}
+
+fn great_ladies(){
+    println!("Hello ladies {wife}, {secondgf}, {smate}, {blazzmate}!", wife="Ada", secondgf="Sarah", smate="Aminatou", blazzmate="Pascalia");
+}
